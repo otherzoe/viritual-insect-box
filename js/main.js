5a417d3418da0d45b2d7b88c4ec58be462e3adff
zoe <zoe@keyte.me>
2023-01-04 19:13:28 -0600

    Create exit button for insect info pages

--- js/main.js
+++ js/main.js
@@ -6,12 +6,27 @@ let insectOneInfo = document.querySelector("#insect-1-info");
 // write generic toggle function ...
 
 let selectInsect = (insect, insectInfo) => {
-	insect.classList.add("active");
-    insectInfo.classList.add("active");
-}
+  insect.classList.toggle("active");
+  insectInfo.classList.toggle("active");
+};
 
 // add event listeners for each insect ...
 
 insectOne.addEventListener("click", () => {
-    selectInsect(insectOne, insectOneInfo);
+  selectInsect(insectOne, insectOneInfo);
+});
+
+let exitButton = document.querySelectorAll("button.exit");
+
+let exitFunction = () => {
+  let active = document.querySelectorAll(".active");
+  active.forEach((e) => {
+    e.classList.remove("active");
+  });
+};
+
+exitButton.forEach((e) => {
+  e.addEventListener("click", () => {
+    exitFunction();
+  });
 });
